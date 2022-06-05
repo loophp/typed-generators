11972dd6391d5b579b59e88bae771a36860b1259
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-06-06 00:19:39 +0200

    Add `IteratorType`.

---
+++ src/Types/Core/IteratorType.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\TypedGenerators\Types\Core;
+
+use Iterator;
+
+use loophp\TypedGenerators\Types\TypeGenerator;
+
+/**
+ * @template TKey
+ * @template T
+ *
+ * @implements TypeGenerator<Iterator<TKey, T>>
+ */
+final class IteratorType implements TypeGenerator
+{
+    /**
+     * @var TypeGenerator<TKey>
+     */
+    private TypeGenerator $key;
+
+    /**
+     * @var TypeGenerator<T>
+     */
+    private TypeGenerator $value;
+
+    /**
+     * @param TypeGenerator<TKey> $k
+     * @param TypeGenerator<T> $v
+     */
+    public function __construct(TypeGenerator $k, TypeGenerator $v)
+    {
+        $this->key = $k;
+        $this->value = $v;
+    }
+
+    /**
+     * @return Iterator<TKey, T>
+     */
+    public function __invoke(): Iterator
+    {
+        while (true) {
+            yield ($this->key)() => ($this->value)();
+        }
+    }
+
+    /**
+     * @param Iterator<TKey, T> $input
+     *
+     * @return Iterator<TKey, T>
+     */
+    public function identity($input): Iterator
+    {
+        return $input;
+    }
+
+    /**
+     * @template WKey
+     * @template W
+     *
+     * @param TypeGenerator<WKey> $k
+     * @param TypeGenerator<W> $v
+     *
+     * @return IteratorType<WKey, W>
+     */
+    public static function new(TypeGenerator $k, TypeGenerator $v): self
+    {
+        return new self($k, $v);
+    }
+}
